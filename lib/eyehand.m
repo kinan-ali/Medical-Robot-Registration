f8d45b5da7cfb9355504851da04f13b93c5505f3
filomorini <filippo.morini@etu.unistra.fr>
2026-01-15 19:46:42 +0100

    Update eyehand.m

--- lib/eyehand.m
+++ lib/eyehand.m
@@ -28,23 +28,9 @@ for ii = 1:nb_configurations
     A(k:k+2,1:3) = listA(1:3,1:3,ii) - eye(3);
     b(k:k+2,1) = Rx * listB(1:3,4,ii) - listA(1:3,4,ii);
 end
-
+% Solving with Moore-Penrose pseudoinverse as it finds tx by
+% minimizing the norm of the residual: min||A*tx - b||^2 
 tx = pinv(A)*b;
 X = [Rx tx; 0 0 0 1];
 
 end
-
-
-% NOTE on solving AX = XB for translation
-% - 1) take AX = XB and split in two equations, one for rotation
-% (solved before) and one for translation: Ra*tx + ta = Rx*tb + tx
-% - 2) Group for tx: (Ra - I) tx = Rx*tb - ta which is in the form Ax = b
-% The structure of the total system becomes: 
-% [Ra1 - I;         [Rx*tb1 - ta1;
-%  Ra2 - I;  * tx =  Rx*tb2 - ta2;
-%  ...               ...
-%  RaN - I]          Rx*tbN - taN]
-% Therefore each movement contributes with a 3 equations block
-% - 3) Solving with Moore-Penrose pseudoinverse as it finds tx by
-% minimizing the norm of the residual: min||A*tx - b||^2 (see cost
-% function)
